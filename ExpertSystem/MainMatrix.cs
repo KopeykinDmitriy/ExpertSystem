8b0b95b21229dcce4b744164ce22ac783d82c74b
Дмитрий Копейкин <d.kopeikin@bright-soft.ru>
2023-10-23 01:19:45 +0500

    1

---
+++ ExpertSystem/MainMatrix.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpertSystem
+{
+    internal class MainMatrix
+    {
+        private readonly int _height = 7;
+        private readonly int _width = 5;
+
+        private List<int> matrix = new List<int>();
+
+        public MainMatrix()
+        {
+            for (int i = 0; i < _height * _width; i++)
+            {
+                matrix.Add(0);
+            }
+        }
+
+        public int GetElement(int x, int y)
+        {
+            if (x >= _height || y >= _width)
+                throw new ArgumentException();
+            return matrix[y + x * _width];
+        }
+        public void SetElement(int x, int y, int value)
+        {
+            if (x >= _height || y >= _width)
+                throw new ArgumentException();
+            matrix[y + x * _width] = value;
+        }
+
+        public void Education(int x, List<int> educationMatrix)
+        {
+            if (educationMatrix == null || educationMatrix.Count != _height)
+                throw new ArgumentException();
+
+            for (int h = 0; h < _height; h++)
+                for (int w = 0; w < _width; w++)
+                {
+                    if (w == x)
+                    {
+                        SetElement(h, w, GetElement(h, w) + educationMatrix[h]);
+                    }
+                    else
+                    {
+                        SetElement(h, w, GetElement(h, w) - educationMatrix[h]);
+                    }
+                }
+        }
+
+        public int Expert(List<int> inputMatrix)
+        {
+            if (inputMatrix == null || inputMatrix.Count != _height)
+                throw new ArgumentException();
+
+            var maxValue = int.MinValue;
+            var targetClass = int.MinValue;
+
+            for (int h = 0; h < _height; h++)
+                for (int w = 0; w < _width; w++)
+                {
+                    var value = 0;
+                    value += GetElement(h, w) * inputMatrix[w];
+                    if (value > maxValue)
+                    {
+                        maxValue = value;
+                        targetClass = h;
+                    }
+                }
+            return targetClass;
+        }
+    }
+}
--- ExpertSystem/MainWindow.xaml.cs
+++ ExpertSystem/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace ExpertSystem
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainMatrix mainMatrix;
+
+        List<Label> labels = new List<Label>();
         public MainWindow()
         {
             InitializeComponent();
+            mainMatrix = new MainMatrix();
+
+            labels.Add(g00); labels.Add(g01); labels.Add(g02); labels.Add(g03); labels.Add(g04);
+            labels.Add(g10); labels.Add(g11); labels.Add(g12); labels.Add(g13); labels.Add(g14);
+            labels.Add(g20); labels.Add(g21); labels.Add(g22); labels.Add(g23); labels.Add(g24);
+            labels.Add(g30); labels.Add(g31); labels.Add(g32); labels.Add(g33); labels.Add(g34);
+            labels.Add(g40); labels.Add(g41); labels.Add(g42); labels.Add(g43); labels.Add(g44);
+            labels.Add(g50); labels.Add(g51); labels.Add(g52); labels.Add(g53); labels.Add(g54);
+            labels.Add(g60); labels.Add(g61); labels.Add(g62); labels.Add(g63); labels.Add(g64);
+
+        }
+
+        private void InitializeMatrix()
+        {
+            for (int w = 0; w < 7; w++)
+                for (int h = 0; h < 5; h++)
+                {
+                    labels[h + w * 5].Content = Convert.ToString(mainMatrix.GetElement(w, h));
+                }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rejim.Content == "Режим обучения")
+            {
+                var inputMatrix = new List<int> { Convert.ToInt32(ComboBoxClass_Copy.Text), Convert.ToInt32(ComboBoxClass_Copy1.Text),
+                                              Convert.ToInt32(ComboBoxClass_Copy2.Text), Convert.ToInt32(ComboBoxClass_Copy3.Text),
+                                              Convert.ToInt32(ComboBoxClass_Copy4.Text), Convert.ToInt32(ComboBoxClass_Copy5.Text),
+                                              Convert.ToInt32(ComboBoxClass_Copy6.Text)};
+                var inputClass = Convert.ToInt32(ComboBoxClass.Text);
+
+                mainMatrix.Education(inputClass, inputMatrix);
+                InitializeMatrix();
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (slider.Value == 1)
+            {
+                rejim.Content = "Режим экспертизы";
+                var brush = new SolidColorBrush(Color.FromRgb(255, 0, 255));
+                rejim.Foreground = brush;
+                ComboBoxClass.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                rejim.Content = "Режим обучения";
+                var brush = new SolidColorBrush(Color.FromRgb(50, 200, 30));
+                rejim.Foreground = brush;
+                ComboBoxClass.Visibility = Visibility.Visible;
+            }
         }
     }
 }
